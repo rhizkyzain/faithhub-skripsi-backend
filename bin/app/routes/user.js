863ff7f2978e6220b9da8f0894134537163d0e15
Rhizky Zain Nabil Subiyantara <rhizky.subiyantara@binus.ac.id>
2024-08-05 17:53:32 +0700

    route fitur add admin

--- bin/app/routes/user.js
+++ bin/app/routes/user.js
@@ -3,7 +3,8 @@
 
     const router = express.Router();
     router.route("/signup").post(authController.registerUser);
+    router.route("/signupAdmin").post(authController.registerAdmin);
     router.route("/login").post(authController.login);
-    // router.route('/logout').post(authController.protect, authController.logout);
+    
     router.route("/myProfile").get(authController.authMiddleware, authController.myProfile);
     module.exports = router;
